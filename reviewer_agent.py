3b07fc1c6f49d5c0404bfc155601d930c63e4cda
azeem95ha <azeem95ha@gmail.com>
2025-04-12 00:54:07 +0200

    Update reviewer_agent.py

--- reviewer_agent.py
+++ reviewer_agent.py
@@ -612,13 +612,13 @@ def submittal_analysis_page(worker: Optional[Any]) -> None:
     )
     input_features = st.text_input("Comparison Features")
 
-    button_disabled = (not worker or not uploaded_files or input_features.strip()=="")
+    button_disabled = (not worker or not uploaded_files)
 
     if st.button("Analyze Submittal", key="analyze_button", disabled=button_disabled):
         try:
             # Step 1: Extract input features
             #st.session_state.input_features = ""
-            if input_features.strip():
+            if input_features.strip() != "":
                 model = initialize_gemini_model().with_structured_output(ComparisonFeatures)
                 st.session_state.input_features = model.invoke(str(input_features)).features
                 logger.info(f"Parsed input features: {st.session_state.input_features}")
